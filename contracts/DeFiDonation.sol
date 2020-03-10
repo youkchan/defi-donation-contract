50c8b81678f7524a17cb72a2705965c984fbb3a2
Yuki Tanaka <yukitanaka@Yuki-no-MacBook-Pro.local>
2020-03-10 18:56:36 +0900

    #3 modify indent

--- contracts/DeFiDonation.sol
+++ contracts/DeFiDonation.sol
@@ -9,24 +9,24 @@ contract DeFiDonation {
   DonationAccountFactory private daf;
   
   constructor(address _usdcAddress, address _cusdcAddress, address _dafAddress) public {
-	  usdc = USDCInterface(_usdcAddress);
-	  cusdc = CTokenInterface(_cusdcAddress);
-	  daf = DonationAccountFactory(_dafAddress);
+    usdc = USDCInterface(_usdcAddress);
+    cusdc = CTokenInterface(_cusdcAddress);
+    daf = DonationAccountFactory(_dafAddress);
   }
 
   function createDonationAccount() public {
-	  require(donationAccounts[msg.sender] == address(0));
-	  address donationAccount = daf.createInstance(address(usdc), address(cusdc), msg.sender);
-	  donationAccounts[msg.sender] = donationAccount;
+    require(donationAccounts[msg.sender] == address(0));
+    address donationAccount = daf.createInstance(address(usdc), address(cusdc), msg.sender);
+    donationAccounts[msg.sender] = donationAccount;
   }
 
   function isAccountExists() public view returns (bool){
-	  return donationAccounts[msg.sender] != address(0);
+    return donationAccounts[msg.sender] != address(0);
   }
 
   function getDonationAccount() public view returns (address) {
-	  require(donationAccounts[msg.sender] != address(0));
-	  return donationAccounts[msg.sender];
+    require(donationAccounts[msg.sender] != address(0));
+    return donationAccounts[msg.sender];
   }
 
 }
--- contracts/DonationAccountFactory.sol
+++ contracts/DonationAccountFactory.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 contract DonationAccountFactory {
   function createInstance(address _usdcAddress, address _cusdcAddress, address _newOwner) public returns (address){
     DonationAccount account = new DonationAccount(_usdcAddress, _cusdcAddress);
-	account.transferOwnership(_newOwner);
+    account.transferOwnership(_newOwner);
     return address(account);
   }
 }
@@ -19,8 +19,8 @@ contract DonationAccount is OpenZeppelinUpgradesOwnable{
   using SafeMath for uint256;
 
   constructor(address _usdcAddress, address _cusdcAddress) public {
-	usdc = USDCInterface(_usdcAddress);
-	cusdc = CTokenInterface(_cusdcAddress);
+    usdc = USDCInterface(_usdcAddress);
+    cusdc = CTokenInterface(_cusdcAddress);
   }
 
   event Deposited(address indexed sender, uint256 amount);
@@ -29,14 +29,14 @@ contract DonationAccount is OpenZeppelinUpgradesOwnable{
 
   function _deposit(uint _amount) public onlyOwner{
     require(usdc.transferFrom(msg.sender, address(this), _amount), "Transfer Fail");
-	emit Deposited(msg.sender, _amount);
+    emit Deposited(msg.sender, _amount);
   }
 
   function supply(uint _amount) public onlyOwner{
     _deposit(_amount);
     usdc.approve(address(cusdc),_amount);
     assert(cusdc.mint(_amount) == 0);
-	emit Supplied(msg.sender, _amount);
+    emit Supplied(msg.sender, _amount);
   }
 
   function addDonateProject(address _projectAddress, uint _amount) public onlyOwner{
@@ -49,24 +49,24 @@ contract DonationAccount is OpenZeppelinUpgradesOwnable{
   }
 
   function donate(address _projectAddress) public onlyOwner{
-	require(donateAmount[_projectAddress] != 0);
+    require(donateAmount[_projectAddress] != 0);
     uint _amount = donateAmount[_projectAddress];
     require(cusdc.redeemUnderlying(_amount) == 0, "Something went wrong");
     require(usdc.transfer(_projectAddress, _amount), "Transfer Fail");
-	uint length = projects.length;
-	for(uint i = 0; i < length; i++ ) {
-		if (projects[i] == _projectAddress) {
-	      delete projects[i];
-		}
-	}
-	delete donateAmount[_projectAddress];
+    uint length = projects.length;
+    for(uint i = 0; i < length; i++ ) {
+      if (projects[i] == _projectAddress) {
+        delete projects[i];
+      }
+    }
+    delete donateAmount[_projectAddress];
   }
 
   function redeem(uint _amount) public onlyOwner{
-	uint length = projects.length;
-	for(uint i = 0; i < length; i++ ) {
+    uint length = projects.length;
+    for(uint i = 0; i < length; i++ ) {
       require(projects[i] == address(0), "You have to donate first");
-	}
+    }
     require(cusdc.redeemUnderlying(_amount) == 0, "Something went wrong");
     require(usdc.transfer(msg.sender, _amount), "Transfer Fail");
   }
